0448aff72ef1249cc3b451331b1babd6695a4034
ibrahimasoryferraz-sys <ibrahimasoryferraz@gmail.com>
2026-08-31 01:46:48 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Storefront from "./Storefront.jsx";
+import Admin from "./Admin.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Storefront />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
